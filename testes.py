b23dd550fe6c5298738619f64f803e8ccf03f549
Renan Igor <renanigor314@gmail.com>
2023-04-21 13:17:08 -0300

    só falta adicionar as informações

--- testes.py
+++ testes.py
@@ -1,6 +1,5 @@
-#TESTES
-
 def formata_cpf(num):
+
     parte_01 = num[0:3]
     parte_02 = num[3:6]
     parte_03 = num[6:9]
@@ -9,40 +8,47 @@ def formata_cpf(num):
     return f'{parte_01}.{parte_02}.{parte_03}-{parte_04}'
 
 
-
 def valida_cpf(cpf):
+
     if len(set(cpf)) == 1:
         return 'NÃO'
 
     num = cpf[0:9]
     soma = 0
     peso = 10
+
     for i in range(0, 9):
         soma += int(num[i]) * peso
         peso -= 1
+
     soma *= 10
     digito_01 = soma % 11 
     if digito_01 > 9:
         digito_01 = 0
 
-    num_com_1_digito = f'{num}{digito_01}'
+    base_2 = (f'{num}{digito_01}')
     soma_2 = 0
     peso_2 = 11
+    
     for i in range (0, 10):
-        soma_2 += int(num_com_1_digito[i]) * peso_2
+        soma_2 += int(base_2[i]) * peso_2
         peso_2 -= 1
+    
     soma_2 *= 10 
     digito_02 = (soma_2 % 11)
+    
     if digito_02 > 9:
         digito_02 = 0
 
     cpf_valido = f'{num}{digito_01}{digito_02}'
+    
     if cpf == cpf_valido:
         return 'SIM'
     else: 
         return 'NÃO'
 
 def formata_cnpj(cnpj):
+    
     parte_01 = cnpj[0:2]
     parte_02 = cnpj[2:5]
     parte_03 = cnpj[5:8]
@@ -51,18 +57,19 @@ def formata_cnpj(cnpj):
 
     return f'{parte_01}.{parte_02}.{parte_03}/{parte_04}-{parte_05}'
 
-
-
 def valida_cnpj(cnpj):
     if len(set(cnpj)) == 1:
-        return 'CNPJ INVÁLIDO'
+        return 'NÃO'
+
     peso = '543298765432'
     base = cnpj[0:12]
     soma = 0
     i = 0
+    
     for numero in base:
         soma += int(numero) * int(peso[i])
         i += 1
+
     digito_01 = 11 - (soma % 11)
 
     if digito_01 >= 10:
@@ -72,6 +79,7 @@ def valida_cnpj(cnpj):
     base_2 = (f'{cnpj[0:12]}{digito_01}')
     soma_2 = 0
     i_2 = 0
+
     for numero in base_2:
         soma_2 += int(numero) * int(peso_2[i_2])
         i_2 += 1
@@ -82,9 +90,8 @@ def valida_cnpj(cnpj):
         digito_02 = 0
 
     cnpj_valido = (f'{base_2}{digito_02}')
-    if cnpj == cnpj_valido:
-        return 'CNPJ É VÁLIDO'
-    else:
-        return 'CNPJ É INVÁLIDO'
 
-print(valida_cnpj('1111111111111'))
+    if cnpj == cnpj_valido:
+        return 'SIM'
+    else:
+        return 'NÃO'
--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from autenticador import app
 from flask import render_template, request, redirect, url_for
-from testes import formata_cpf, valida_cpf
+from testes import formata_cpf, valida_cpf, formata_cnpj, valida_cnpj
 
 @app.route('/')
 def index():
@@ -9,9 +9,17 @@ def index():
 @app.route('/resultado', methods=['POST', 'GET'])
 def resultado():
     cpf = request.form.get('cpf')
-    if cpf is not None:
-        cpf_formatado = formata_cpf(cpf)
-        cpf_validado = valida_cpf(cpf)
-        return render_template('template.html', cpf_formatado=cpf_formatado, cpf_validado=cpf_validado)
+    cnpj = request.form.get('cnpj')
+    
+    if cpf or cnpj:
+        cpf_formatado = formata_cpf(cpf) if cpf else None
+        cpf_validado = valida_cpf(cpf) if cpf else None
+        
+        cnpj_formatado = formata_cnpj(cnpj) if cnpj else None
+        cnpj_validado = valida_cnpj(cnpj) if cnpj else None
+
+
+        return render_template('template.html', cpf_formatado=cpf_formatado, cpf_validado=cpf_validado, cnpj_formatado=cnpj_formatado, cnpj_validado=cnpj_validado)
     else:
         return redirect(url_for('index'))
+
